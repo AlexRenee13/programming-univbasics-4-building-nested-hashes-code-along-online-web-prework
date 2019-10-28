2c903e641a3978bca4e3399f72fcb86f4f575099
Alexis Palacios <alexis1994662@yahoo.com>
2019-10-28 20:03:14 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,9 +2,6 @@ def first_challenge
    epic_tragedy = {
      :montague => {}
      :capulet => {
-     :patriarch => {name: "Lord Capulet", age: "50"},
-     :matriarch => {name: "Lady Capulet", age: "51"},
-     :heroine => {name: "Juliet", age: "15", status: "alive"},
-     :heroine_friends => []
+     :patriarch => {
    }
 end
